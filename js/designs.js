9003a724c6232f3d5ae9e75a410ace58986ac4ce
Karen Kepner <kkepner14@gmail.com>
2018-08-19 21:27:07 -0600

    Add js to collect height and width for grid.

--- js/designs.js
+++ js/designs.js
@@ -1,3 +1,25 @@
-makeGrid() {
-	
+//Select color.
+let color = document.querySelector('color-picker');
+
+
+const submitGrid = document.querySelector('#submit-grid');
+//Set grid size.
+submitGrid.addEventListener('click', function(){
+	let height = document.querySelector("#height").value;
+	let width = document.querySelector("#width").value;
+	console.log('height:' + height);
+	console.log('width:' + width);
+	//alert('height:' + height + 'width:' + width);
+	submitGrid.preventDefault();
+	makeGrid();
+})
+
+//document.querySelector("")
+//on Submit of both height and width, assign those values to height and width.
+
+
+function makeGrid() {
+	console.log("Make a grid.");
+	const theGrid = document.querySelector('#the-grid');
+	//theGrid.createElement('table');
 }

be2b58e90d5d874774836e6a066eef6cebf58fe6
Karen Kepner <kkepner14@gmail.com>
2018-08-21 21:36:46 -0600

    Add JS to collect data from inputs.

--- js/designs.js
+++ js/designs.js
@@ -1,22 +1,22 @@
 //Select color.
-let color = document.querySelector("#color-picker");
+let color = document.getElementById("color-picker");
 
-let theGrid = document.querySelector("#the-grid");
+let theGrid = document.getElementById("the-grid");
 
 const submitGrid = document.querySelector('#submit-grid');
 let height = 5 //document.querySelector("#height");
 let width =  10 //document.querySelector("#width");
 
 submitGrid.addEventListener('click', function makeGrid() {
-
-	let height = document.querySelector("#height");
-	let width =  document.querySelector("#width");
+	height = document.getElementById("height");
+	width =  document.getElementById("width");
 	for (let i = 0; i < width; i++) {
 	 	theGrid.insertAdjacentHTML('afterbegin', '<tr>table row</tr>');
 	}
 	for (let j = 0; j < height; j++) {
 	 	theGrid.insertAdjacentHTML('afterbegin', '<td>table detail</td>');
 	}
+	theGrid.preventDefault();
 });
 	//build the grid with a for loop that creates
 	// the elements of a table that has css to show gridlines.
